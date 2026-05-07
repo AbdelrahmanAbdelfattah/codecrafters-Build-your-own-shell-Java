c78535c28a46b1c57561ec7e3a03ddcabb06dd3b
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2026-05-07 23:35:19 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
          Scanner scanner = new Scanner(System.in);
          String command = scanner.nextLine();
-         System.out.print(command+ " command not found");
+         System.out.print(command+ ": command not found");
     }
 }
