291c5f276c6434b930a4bb219ba9b9df10aac63a
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2026-05-11 10:02:33 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,25 @@ import java.io.File;
 import java.util.regex.Pattern;
 
 public class Main {
+
+
+    public static String getPath(String command){
+        String pathVariable = System.getenv("PATH");
+
+        // This will safely split by ';' on Windows and ':' on Linux
+        String[] directories = pathVariable.split(Pattern.quote(File.pathSeparator));
+
+        for (String directory : directories) {
+            String PATH = directory +File.separator+command;
+            File file = new File(PATH);
+            if (file.exists() && file.canExecute()) {
+                return file.getAbsolutePath();
+            }
+        }
+        return null;
+    }
+
+
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
 
@@ -21,33 +40,24 @@ public class Main {
              }
              else if (command.startsWith("type")) {
                  String result = command.replaceFirst("^type\\s+", "");
-                 boolean founded = false;
+
+                 // Splits the string by spaces to separate the command from the arguments
+                 String[] commandParts = command.split("\\s+");
+
                  if (result.equals("type") || result.equals("echo") ||  result.equals("exit")) {
                      System.out.println(result+ " is a shell builtin");
                  }
                  else {
-                     String pathVariable = System.getenv("PATH");
-
-                     // This will safely split by ';' on Windows and ':' on Linux
-                     String[] directories = pathVariable.split(Pattern.quote(File.pathSeparator));
-
-
-
-                     for (String directory : directories) {
-                         String PATH = directory +File.separator+result;
-                         if (new File(PATH).exists() && new File(PATH).canExecute()) {
-                             System.out.println(result+ " is " +PATH);
-                             founded = true;
-                             break;
-                         }
+                     String fullPath = getPath(result);
+                     if (fullPath != null) {
+                         // Now you have the path needed to start the process!
+                         ProcessBuilder pb = new ProcessBuilder(commandParts);
+                         pb.inheritIO().start().waitFor();
+                     } else {
+                         System.out.println(command + ": command not found");
                      }
-                     if (!founded) {
-                         System.out.println(result+ ": not found");
-                     }
-
 
                  }
-
              }
              else {
                  System.out.println(command+ ": command not found");
@@ -57,3 +67,4 @@ public class Main {
 
     }
 }
+
